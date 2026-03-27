ff0bedd66a43090076dbdb5f10eff5ba95abb124
zachthag <zgass3743@sdsu.edu>
2026-03-27 00:12:53 -0700

    Added template files

--- ArrayStack.h
+++ ArrayStack.h
@@ -1,1 +1,37 @@
-//initial arraystack
+#ifndef ARRAYSTACK_H
+#define ARRAYSTACK_H
+
+#include <vector>
+#include <stdexcept>
+
+template <typename T>
+class ArrayStack {
+private:
+    std::vector<T> data;
+
+public:
+    void push(const T& value) {
+        // TODO
+    }
+
+    void pop() {
+        // TODO
+    }
+
+    T top() const {
+        // TODO
+        return T();
+    }
+
+    bool empty() const {
+        // TODO
+        return true;
+    }
+
+    int size() const {
+        // TODO
+        return 0;
+    }
+};
+
+#endif
